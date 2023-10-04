6080efb7cdc7370c60f57a63665e7b5d931e2f43
Kalle Tolonen <68230354+kalletolonen@users.noreply.github.com>
2023-10-04 09:37:10 +0300

    Ex8. cities

--- ex8/cities.py
+++ ex8/cities.py
@@ -9,38 +9,50 @@
 #    konstruktori, jolle annetaan kaupunkien määrä
 #    add_road lisää tien kahden kaupungin välille
 #    has_route tarkastaa, onko kahden kaupungin välillä reittiä
-
 class City:
     def __init__(self, name):
         self.name = name
-        self.roads = set()
+        self.neighbors = set()
 
     def __str__(self):
-        return f"City: {self.name} Roads: {self.roads}"
+        return f"City: {self.name} Roads to cities: {self.neighbors}"
     
 
 class Cities:
     def __init__(self,n):
-        self.cities = [City({i}) for i in range(n + 1)]
+        self.cities = [City(i) for i in range(n + 1)]
 
     def add_road(self,a,b):
-        self.cities[a].roads.add(b)
+        self.cities[a].neighbors.add(b)
+        self.cities[b].neighbors.add(a)
 
     def has_route(self,a,b):
-        if a in self.cities[a].roads or b in self.cities[b].roads:
+        visited = set()
+        return self.search_for_path(a, b, visited)
+    
+    def search_for_path(self, current, target, visited):
+        if current == target:
             return True
         
+        visited.add(current)
+        
+        for neighbor in self.cities[current].neighbors:
+            if neighbor not in visited:
+                if self.search_for_path(neighbor, target, visited):
+                    return True
+                
         return False
 
-# Gotta account for links to nodes far away, not just the next town over
-if __name__ == "__main__":
+def main():
     c = Cities(5)
-    print(c.cities)
-    c.add_road(1,2)
     c.add_road(1,2)
     c.add_road(1,3)
     c.add_road(4,5)
     print(c.has_route(1,5)) # False
     c.add_road(3,4)
     print(c.has_route(1,5)) # True
+    #for city in c.cities:
+    #    print(f"City: {city.name} Roads: {city.neighbors}")
 
+if __name__ == "__main__":
+    main()
